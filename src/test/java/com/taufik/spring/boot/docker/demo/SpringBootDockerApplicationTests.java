0e6dc31e4b782b5868e52c437289b9640170f242
ttaufik1903 <54972223+ttaufik1903@users.noreply.github.com>
2020-12-22 17:34:51 +0700

    init project

---
+++ src/test/java/com/taufik/spring/boot/docker/demo/SpringBootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taufik.spring.boot.docker.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
